ad54b0afa6493537bf6179295dbc3841f83043e2
nicolasclinca <nicolasclinca2@gmail.com>
2025-06-02 11:18:34 +0200

    .

--- src/evaluation.py
+++ src/evaluation.py
@@ -135,8 +135,7 @@ def T5_text_generator(
 #def model_loading(expected_outputs, retrieve_results, test_data):
 def model_loading():
     # Load the test data
-    with open("../data/nq-dev-kilt.jsonl", "r", encoding="utf-8") as f:
-        test_data = [json.loads(line) for line in f]
+    test_data = "../data/nq-dev-kilt.jsonl"
 
     expected_outputs, retrieve_results = retrieval_results(filename=test_data)
     model_path = "../models/finetuned_t5_model_fid"
